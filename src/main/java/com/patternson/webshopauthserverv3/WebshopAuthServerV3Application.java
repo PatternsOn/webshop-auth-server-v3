f7b7924eb7ecaa3a13bab19f34718255b43a4a09
TobiasPettersson <pettersson.tobias84@gmail.com>
2018-03-20 10:35:49 +0100

    init commit

---
+++ src/main/java/com/patternson/webshopauthserverv3/WebshopAuthServerV3Application.java
@@ -0,0 +1,12 @@
+package com.patternson.webshopauthserverv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopAuthServerV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopAuthServerV3Application.class, args);
+	}
+}
---
+++ src/test/java/com/patternson/webshopauthserverv3/WebshopAuthServerV3ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.patternson.webshopauthserverv3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebshopAuthServerV3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
